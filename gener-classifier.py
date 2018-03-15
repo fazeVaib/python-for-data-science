404b77de5d7878a072b572c4130969018364eb36
fazevaib <vaibhavvats123@gmail.com>
2018-03-15 12:55:32 +0530

    added GaussianNB

--- gener-classifier.py
+++ gener-classifier.py
@@ -1,9 +1,10 @@
 import numpy
-from sklearn import gaussian_process, metrics, neighbors, tree
+from sklearn import gaussian_process, metrics, neighbors, tree, naive_bayes
 
 clf = tree.DecisionTreeClassifier()
 clf2 = neighbors.KNeighborsClassifier()
 clf3 = gaussian_process.GaussianProcessClassifier()
+clf4 = naive_bayes.GaussianNB()
 
 
 # [height, weight, shoe_size]
@@ -17,6 +18,7 @@ Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
 clf = clf.fit(X, Y)
 clf2 = clf2.fit(X,Y)
 clf3 = clf3.fit(X, Y)
+clf4 = clf4.fit(X, Y)
 
 prediction = clf.predict(X)
 acc = metrics.accuracy_score(Y, prediction) * 100
@@ -24,13 +26,15 @@ prediction2 = clf2.predict(X)
 acc2 = metrics.accuracy_score(Y, prediction2) * 100
 prediction3 = clf3.predict(X)
 acc3 = metrics.accuracy_score(Y, prediction3) * 100
-
+prediction4 = clf4.predict(X)
+acc4 = metrics.accuracy_score(Y, prediction4) * 100
 
 print("Decision Tree: ", prediction, " Accuracy: ", acc)
 print("Nearest Neighbor: ", prediction2, " Accuracy: ", acc2)
 print("Guassian Process: ", prediction3, " Accuracy: ", acc3)
+print("Guassian Naive Bayes: ", prediction4, " Accuracy: ", acc4)
 
-index = numpy.argmax([acc, acc2, acc3])
+index = numpy.argmax([acc, acc2, acc3, acc4])
 print('\n', index)
-classifiers = {0:'Decision Tree', 1:'Nearest Neighbor', 2:'Guassian Process'}
+classifiers = {0:'Decision Tree', 1:'Nearest Neighbor', 2:'Guassian Process', 3:'Gaussian NB'}
 print('Best gender classifier is: {}'.format(classifiers[index]))
